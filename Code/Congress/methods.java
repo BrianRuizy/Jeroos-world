d5cb2d89b3e1303839908f400e149b2cee165128
Brian Ruiz <brianruiz123@yahoo.com>
2019-06-24 10:00:00 -0500

    Update methods.java

--- Code/Congress/methods.java
+++ Code/Congress/methods.java
@@ -1,18 +1,37 @@
-method findDoor() {
-  //-Follow the water on your 
-  //right until to get to an
-  // opening.
-  while( isWater( RIGHT ) )
-  {
-    hop();
-  }
+  
+     method findDoor() 
+     {
+       //-Follow the water on your 
+       //right until to get to an
+       // opening.
+       while( isWater( RIGHT ) )
+       {
+         hop();
+       }
+     
+       //-Turn _____.
+       turn( RIGHT );
+     }
+    // ------------------------------------------------- 
+     
+     method findFlower()
+     {
+        while (! isFlower(HERE))
+        {
+             hop();
+        }
+     
+        if (isFlower(HERE))
+             pick();    
+    } 
+     // ------------------------------------------------- 
+    
+     method findNet()
+     {
+        while (! isNet(AHEAD))
+        {
+             hop();
+        }
+    } 
+     // ------------------------------------------------- 
 
-  //-Turn _____.
-  turn( RIGHT );
-}
-
-method findFLower(){
-
-
-
-}
