aebcd60e5ac71c4cf0c1bfb166be278fcc7bc2ec
Brian Ruiz <brianruiz123@yahoo.com>
2019-06-24 09:22:57 -0500

    Create methods.java

---
+++ Code/Congress/methods.java
@@ -0,0 +1,18 @@
+method findDoor() {
+  //-Follow the water on your 
+  //right until to get to an
+  // opening.
+  while( isWater( RIGHT ) )
+  {
+    hop();
+  }
+
+  //-Turn _____.
+  turn( RIGHT );
+}
+
+method findFLower(){
+
+
+
+}
