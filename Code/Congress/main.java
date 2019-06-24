e17fed2b33e0fa403d7b474133f4f0fe161e5684
Brian Ruiz <brianruiz123@yahoo.com>
2019-06-24 09:22:29 -0500

    Create main.java

---
+++ Code/Congress/main.java
@@ -0,0 +1,12 @@
+method main()
+{
+Jeroo Obama = new Jeroo(2,0,EAST);
+Jeroo Bush = new Jeroo(0,19,SOUTH);
+Jeroo Clinton = new Jeroo(23,4,NORTH);
+Jeroo Reagan = new Jeroo(21,23,WEST);
+
+Obama.findDoor();  // blue
+Bush.findDoor();  // orange
+Clinton.findDoor();  // pink
+Reagan.findDoor();  // green
+}
